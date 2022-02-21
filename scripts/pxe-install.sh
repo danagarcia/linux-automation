ce6752126fa2ad8640f1fca33d481ebfc99b0840
Dana Garcia <dana_sean_garcia@live.com>
2022-02-21 15:19:00 -0800

    Updating for OEL8 and fixing issues with permissions and sed

--- scripts/pxe-install.sh
+++ scripts/pxe-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir /isos && cd /isos
-scp dsgarcia@10.0.1.1:/home/dsgarcia/Documents/isos/{oel79.iso,oel85.iso} /isos/
+mkdir /var/isos && cd /var/isos
+scp dsgarcia@10.0.1.1:/home/dsgarcia/Documents/isos/{oel79.iso,oel85.iso} /var/isos/
 yum install dhcp-server tftp-server vsftpd syslinux -y
 mkdir /var/ftp/pub/ks && cd /var/ftp/pub/ks
 scp dsgarcia@10.0.1.1:/home/dsgarcia/Documents/ks/{oel7_stig.ks,oel8_stig.ks} ./
@@ -28,8 +28,8 @@ next-server 10.0.1.2;
 filename "pxelinux.0";
 }
 EOF
-sed 's/anonymous_enable=NO/anonymous_enable=YES/g' /etc/vsftpd/vsftpd.conf
-sed 's/write_enable=YES/write_enable=NO/g' /etc/vsftpd/vsftpd.conf
+sed -i 's/anonymous_enable=NO/anonymous_enable=YES/g' /etc/vsftpd/vsftpd.conf
+sed -i 's/write_enable=YES/write_enable=NO/g' /etc/vsftpd/vsftpd.conf
 systemctl start dhcpd.service && systemctl enable dhcpd.service
 systemctl start tftp.service && systemctl enable tftp.service
 systemctl enable vsftpd.service && systemctl start vsftpd.service
@@ -44,21 +44,34 @@ cp -v /usr/share/syslinux/libutil.c32 /var/lib/tftpboot/
 cp -v /usr/share/syslinux/libcom32.c32 /var/lib/tftpboot/
 mkdir /var/lib/tftpboot/pxelinux.cfg
 mkdir -p /var/lib/tftpboot/networkboot/oel7
+mkdir -p /var/lib/tftpboot/networkboot/oel8
 mkdir /var/ftp/pub/oel7
-mkdir /iso_image
-mount -o loop /isos/oel79.iso /iso_image
-cp -rf /iso_image/* /var/ftp/pub/oel7
+mkdir /var/ftp/pub/oel8
+mkdir /iso_image/oel7
+mkdir /iso_image/oel8
+mount -o loop /isos/oel79.iso /iso_image/oel7
+mount -o loop /isos/oel85.iso /iso_image/oel8
+cp -rf /iso_image/oel7/* /var/ftp/pub/oel7
+cp -rf /iso_image/oel8/* /var/ftp/pub/oel8
 cp /var/ftp/pub/oel7/images/pxeboot/{initrd.img,vmlinuz} /var/lib/tftpboot/networkboot/oel7/
-chmod 777 /var/lib/tftpboot/*
-cat >/var/lib/tftpboot/pxelinux.cfg/default << EOF
+cp /var/ftp/pub/oel8/images/pxeboot/{initrd.img,vmlinuz} /var/lib/tftpboot/networkboot/oel8/
+cat >> /var/lib/tftpboot/pxelinux.cfg/default << EOF
 default menu.c32
 prompt 0
 timeout 30
 menu title OCI ONSR PXE Menu
-label Install Oracle Linux 7.9
+
+label Install Oracle Linux 7.9 - STIG
 kernel /networkboot/oel7/vmlinuz
 append initrd=/networkboot/oel7/initrd.img inst.repo=ftp://10.0.1.2/pub/oel7 inst.ks=ftp://10.0.1.2/pub/ks/oel7_stig.ks
+
+label Install Oracle Linux 8.5 - STIG
+kernel /networkboot/oel8/vmlinuz
+append initrd=/networkboot/oel8/initrd.img inst.repo=ftp://10.0.1.2/pub/oel8 inst.ks=ftp://10.0.1.2/pub/ks/oel8_stig.ks
 EOF
-chcon -R -t tftpdir_rw_t /var/lib/tftpboot/
+chmod 777 -R /var/lib/tftpboot
+chmod 777 -R /var/ftp
+chcon -R -t tftpdir_rw_t /var/lib/tftpboot
 chcon -R -t public_content_t /var/ftp
-umount /iso_image
+umount /iso_image/oel7
+umount /iso_image/oel8
